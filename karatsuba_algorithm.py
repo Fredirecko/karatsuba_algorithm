26b8bb208b53eefb4f8ff848b32147a4709d5a14
Fredirecko <avspedaliere@gmail.com>
2020-05-06 18:51:20 -0700

    completed basic functionality of karatsuba algorithm

--- karatsuba_algorithm.py
+++ karatsuba_algorithm.py
@@ -19,28 +19,52 @@
 
 
 def karatsubaAlgorithm(x,y):
-    return(countDigitsCheck(x,y))
+    if countDigitsCheck(x,y):
+        return x * y
+    else:
 
+        x = str(x)
+        y = str(y)
 
+        n = (int(len(x)/2))
+        m = (int(len(y)/2))
 
+        parts_x = [x[i:i+n] for i in range(0, len(x), n)]
+        parts_y = [y[i:i+m] for i in range(0, len(y), m)]
+
+        print(parts_x)
+        print(parts_y)
+
+        a,b = int(parts_x[0]), int(parts_x[1])
+        c,d = int(parts_y[0]), int(parts_y[1])
+
+        print(a)
+        print(b)
+        print(c)
+        print(d)
+
+    step_one = a*c
+    print(step_one)
+    step_two = b*d
+    print(step_two)
+    step_three = (a+b)*(c+d)
+    print(step_three)
+    step_four = step_three - step_one - step_two
+    print(step_four)
+    step_five = (step_one*10000)+(step_four*100)+step_two
+
+    return step_five
 
 
 
 def countDigitsCheck(one, two):
     count_check_one = countDigits(one)
     count_check_two = countDigits(two)
-    a = one
-    b = two
-    Karatsuba = "Run Karatsuba Algorithm"
-    print(count_check_one)
-    print(count_check_two)
+
     if count_check_one == 1 and count_check_two == 1:
-        answer = a * b
-        return(answer)
+        return True
     else:
-        return(Karatsuba)
-
-
+        return False
 
 def countDigits(number):
     Number = number
@@ -54,5 +78,7 @@ def countDigits(number):
 
 
 
-check_function = karatsubaAlgorithm(100,9)
+check_function = karatsubaAlgorithm(5678,1234)
+test_function = karatsubaAlgorithm(9,9)
 print(check_function)
+print(test_function)
